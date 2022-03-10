1ee4e25c964bfa8b9f144093b1ed6739496c4c16
Mahfod <mahfod.dev@gmail.com>
2022-03-10 09:05:11 +0100

    add alertContext ReposList

--- src/components/context/github/GithubContext.js
+++ src/components/context/github/GithubContext.js
@@ -9,32 +9,93 @@ const GithubContext = createContext()
 export const GithubProvider = ({ children }) => {
 	const initialState = {
 		users: [],
+		user: {},
+		repos: [],
 		loading: true,
 	}
 
 	const [state, dispatch] = useReducer(githubReducer, initialState)
 
-	const fecthUsers = async () => {
+	const searchUsers = async (text) => {
 		setLoading()
-		const response = await fetch(`${GITHUB_URL}/users`, {
+
+		const params = new URLSearchParams({
+			q: text,
+		})
+
+		const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
 			headers: {
 				Authorization: `${GITHUB_TOKEN}`,
 			},
 		})
 
-		const data = await response.json()
+		const { items } = await response.json()
 
 		dispatch({
 			type: 'GET_USERS',
+			payload: items,
+		})
+	}
+	const singleUser = async (login) => {
+		setLoading()
+
+		const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+			headers: {
+				Authorization: `${GITHUB_TOKEN}`,
+			},
+		})
+
+		if (response.status === 404) {
+			window.location('/notfound')
+		} else {
+			const data = await response.json()
+
+			dispatch({
+				type: 'GET_USER',
+				payload: data,
+			})
+		}
+	}
+	const getRepos = async (login) => {
+		setLoading()
+
+		const params = new URLSearchParams({
+			sort: 'created',
+			per_page: 10,
+		})
+
+		const response = await fetch(
+			`${GITHUB_URL}/users/${login}/repos?${params}`,
+			{
+				headers: {
+					Authorization: `${GITHUB_TOKEN}`,
+				},
+			}
+		)
+
+		const data = await response.json()
+
+		dispatch({
+			type: 'GET_REPOS',
 			payload: data,
 		})
 	}
 
 	const setLoading = () => dispatch({ type: 'SET_LOADING' })
+	const clearUsers = () => dispatch({ type: 'SET_CLEAR' })
 
 	return (
 		<GithubContext.Provider
-			value={{ users: state.users, loading: state.loading, fecthUsers }}>
+			value={{
+				users: state.users,
+				loading: state.loading,
+				user: state.user,
+				repos: state.repos,
+				searchUsers,
+				clearUsers,
+				singleUser,
+				getRepos,
+			}}>
 			{children}
 		</GithubContext.Provider>
 	)
--- src/components/context/github/GithubReducer.js
+++ src/components/context/github/GithubReducer.js
@@ -2,9 +2,16 @@ const githubReducer = (state, action) => {
 	switch (action.type) {
 		case 'GET_USERS':
 			return { ...state, users: action.payload, loading: true }
+
+		case 'GET_USER':
+			return { ...state, user: action.payload, loading: true }
+		case 'GET_REPOS':
+			return { ...state, repos: action.payload, loading: true }
 		case 'SET_LOADING': {
 			return { ...state, loading: false }
 		}
+		case 'SET_CLEAR':
+			return { ...state, users: [], loading: true }
 
 		default:
 			return state
